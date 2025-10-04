4da2cd7aa74f92a2a7ab81cdc3dc346f4a778b9c
tomy2003river <tomasarrestegui@gmail.com>
2025-10-03 23:39:32 -0300

    - Se añadió un estado `carrito` en App.js para almacenar los productos. - Se creó la función `addToCart` que agrega un producto al carrito. - Se pasó la prop `cartCount` al componente Navbar para mostrar la cantidad. - Se pasó la función `addToCart` al componente Producto para que pueda añadir productos.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import { useState } from "react";
 import Home from "./pages/Home";
 import Productos from "./pages/Productos";
 import Producto from "./pages/Producto";
 import Contacto from "./pages/Contacto";
+import Navbar from "./inc/Navbar";
+
 
 function App() {
+  // 1) Estado global del carrito en App
+  const [carrito, setCarrito] = useState([]);
+
+  // 2) Función para añadir producto al carrito
+  const addToCart = (producto) => {
+    setCarrito([...carrito, producto]);
+  };
+
   return (
     <Router>
+      {/* 3) Navbar recibe la cantidad de productos */}
+      <Navbar cartCount={carrito.length} />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/productos" element={<Productos />} />
-        <Route path="/producto/:id" element={<Producto />} />
+        {/* Le pasamos addToCart a Producto */}
+        <Route path="/producto/:id" element={<Producto addToCart={addToCart} />} />
         <Route path="/contacto" element={<Contacto />} />
       </Routes>
     </Router>
