fd4a36a33795c490857d304c6d9e1f341c2bf17f
mrmetapod2 <hectormzacrias@gmail.com>
2025-10-22 01:25:40 -0300

    React routes completas

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import Productos from "./pages/Productos";
 import ProductDetail from "./pages/Producto";
@@ -8,48 +9,28 @@ import Navbar from "./componentes/Navbar.inc";
 import Footer from "./componentes/footer.inc";
 
 function App() {
-  // Estado global del carrito
   const [carrito, setCarrito] = useState([]);
 
-  // Estado para manejar "navegación"
-  const [pagina, setPagina] = useState("home");
-
-  // Si quieres mostrar un producto específico
-  const [productoSeleccionado, setProductoSeleccionado] = useState(null);
-
   const addToCart = (producto) => {
-    setCarrito([...carrito, producto]);
+    setCarrito((prev) => [...prev, producto]);
   };
-
-  // Función de navegación
-  const navigate = (paginaDestino, producto = null) => {
-    setPagina(paginaDestino);
-    console.log(producto);
-    setProductoSeleccionado(producto);
-    console.log(productoSeleccionado);
-  };
-  
 
   return (
-    <>
-      {/* Navbar recibe navigate y cantidad del carrito */}
-      <Navbar cartCount={carrito.length} navigate={navigate} />
+    <Router>
+      <Navbar cartCount={carrito.length} />
 
-      {/* Renderizado condicional */}
-      {pagina === "home" && <Home navigate={navigate} />}
-      {pagina === "productos" && <Productos navigate={navigate} />}
-      {pagina === "producto" && (
-        <ProductDetail
-          producto={productoSeleccionado}
-          addToCart={addToCart}
-          navigate={navigate}
-          
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route
+          path="/producto/:id"
+          element={<ProductDetail addToCart={addToCart} />}
         />
-      )}
-      {pagina === "contacto" && <Contacto />}
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
 
       <Footer />
-    </>
+    </Router>
   );
 }
 
